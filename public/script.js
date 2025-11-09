79bcabf6c6c7cfdc11b7439298c7447c8d64b978
Suprajna <suprasubedi11@gmail.com>
2025-11-09 10:41:24 +0545

    only one vote can be cast by each browser client

--- public/script.js
+++ public/script.js
@@ -9,3 +9,11 @@ socket.on("updateVotes", (votes) => {
   document.getElementById("voteB").innerText = votes.B;
   document.getElementById("voteC").innerText = votes.C;
 });
+
+let userId = localStorage.getItem("pollUserId");
+if (!userId) {
+  userId = crypto.randomUUID();
+  localStorage.setItem("pollUserId", userId);
+}
+
+socket.emit("register", userId);
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 
 // poll data
 let votes = { A: 0, B: 0, C: 0 };
+const votedUsers = new Set();
 
 io.on("connection", (socket) => {
   console.log(`A user connected`);
@@ -17,10 +18,17 @@ io.on("connection", (socket) => {
   //send current votes to new user
   socket.emit("updateVotes", votes);
 
+  //register id for client
+  socket.on("register", (userId) => {
+    socket.userId = userId;
+  });
+
   //when a user votes
   socket.on("vote", (option) => {
+    if (!socket.userId || votedUsers.has(socket.userId)) return;
     if (votes[option] !== undefined) {
       votes[option]++;
+      votedUsers.add(socket.userId);
       io.emit("updateVotes", votes); //broadcast to all
     }
   });
